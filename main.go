f5058b7fbf5d6ba485de2bdf15afb6f033602764
ivahaev <ivahaev@gmail.com>
2016-06-23 21:43:52 +0500

    Version update: 0.0.11

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
@@ -15,33 +16,43 @@ import (
 var (
 	buildTime string
 	gitHash   string
-	version   = "0.0.10"
+	version   = "0.0.11"
 )
 
 func main() {
 	if os.Getenv("BLANK_DEBUG") != "" {
 		log.SetLevel(log.DebugLevel)
+		settings.DevMode = true
 	}
 	var srAddress *string
-	var devMode *bool
+	var verFlag *bool
 
 	rootCmd := &cobra.Command{
 		Use:   "router",
 		Short: "Router for Blank platform",
 		Long:  "The next generation of web applications. This is the router subsytem.",
 		Run: func(cmd *cobra.Command, args []string) {
+			if *verFlag {
+				printVersion()
+				return
+			}
 			log.Info("Router started")
 			settings.SRAddress = *srAddress
 			settings.SRHTTPAddress = "http:" + strings.TrimPrefix(*srAddress, "ws:")
-			settings.DevMode = *devMode
 			intranet.Init()
 		},
 	}
 	srAddress = rootCmd.PersistentFlags().StringP("service-registry", "s", "ws://localhost:1234", "Service registry uri")
-	devMode = rootCmd.PersistentFlags().BoolP("dev-mode", "d", false, "Development mode")
+	verFlag = rootCmd.PersistentFlags().BoolP("version", "v", false, "Prints version and exit")
 
 	if err := rootCmd.Execute(); err != nil {
 		println(err.Error())
 		os.Exit(-1)
 	}
+}
+
+func printVersion() {
+	fmt.Printf("Build time:  		%s\n", buildTime)
+	fmt.Printf("Commit hash: 		%s\n", gitHash)
+	fmt.Printf("Version:     		%s\n", version)
 }

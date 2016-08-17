ed96495a4f816e6927acbc92c2df7a1f6b22987b
ivahaev <ivahaev@gmail.com>
2016-08-17 15:17:56 +0500

    errors updated

--- berrors/berrors.go
+++ berrors/berrors.go
@@ -2,9 +2,10 @@ package berrors
 
 import "errors"
 
+// Errors
 var (
-	ErrNotConnected     = errors.New("Not connected")
-	ErrError            = errors.New("Unknown error")
-	ErrInvalidArguments = errors.New("Invalid arguments")
-	ErrForbidden        = errors.New("Access denied")
+	ErrNotConnected     = errors.New("not connected")
+	ErrError            = errors.New("unknown error")
+	ErrInvalidArguments = errors.New("invalid arguments")
+	ErrForbidden        = errors.New("access denied")
 )
